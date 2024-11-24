45b63f86f57677e9f18246a90096b43c52b13731
zimoluo <zimo_luo@outlook.com>
2024-11-24 17:29:03 -0500

    1.2.1

--- components/mainPage/backgroundAnimations/pixelland/PixellandAnimatedBackground.tsx
+++ components/mainPage/backgroundAnimations/pixelland/PixellandAnimatedBackground.tsx
@@ -12,8 +12,64 @@ export default function PixellandAnimatedBackground() {
         style={{
           backgroundImage: `url("./theme/animated-background/pixelland/sun/outer-1.png")`,
         }}
-        className={`${spritesStyle.sun} fixed pointer-events-none select-none -z-20`}
+        className={`${
+          spritesStyle.sun
+        } fixed pointer-events-none select-none -z-20 ${
+          settings.backgroundRichness === "rich" ? spritesStyle.outerSun1 : ""
+        }`}
       />
+      {settings.backgroundRichness === "rich" && (
+        <>
+          <div
+            style={{
+              backgroundImage: `url("./theme/animated-background/pixelland/sun/outer-2.png")`,
+            }}
+            className={`${spritesStyle.sun} fixed pointer-events-none select-none -z-20 ${spritesStyle.outerSun2}`}
+          />
+          <div
+            style={{
+              backgroundImage: `url("./theme/animated-background/pixelland/sun/outer-3.png")`,
+            }}
+            className={`${spritesStyle.sun} fixed pointer-events-none select-none -z-20 ${spritesStyle.outerSun3}`}
+          />
+          <div
+            style={{
+              backgroundImage: `url("./theme/animated-background/pixelland/sun/outer-4.png")`,
+            }}
+            className={`${spritesStyle.sun} fixed pointer-events-none select-none -z-20 ${spritesStyle.outerSun4}`}
+          />
+          <div
+            style={{
+              backgroundImage: `url("./theme/animated-background/pixelland/sun/inner-1.png")`,
+            }}
+            className={`${spritesStyle.sun} fixed pointer-events-none select-none -z-20 ${spritesStyle.innerSun1}`}
+          />
+          <div
+            style={{
+              backgroundImage: `url("./theme/animated-background/pixelland/sun/inner-2.png")`,
+            }}
+            className={`${spritesStyle.sun} fixed pointer-events-none select-none -z-20 ${spritesStyle.innerSun2}`}
+          />
+          <div
+            style={{
+              backgroundImage: `url("./theme/animated-background/pixelland/sun/inner-3.png")`,
+            }}
+            className={`${spritesStyle.sun} fixed pointer-events-none select-none -z-20 ${spritesStyle.innerSun3}`}
+          />
+          <div
+            style={{
+              backgroundImage: `url("./theme/animated-background/pixelland/sun/inner-4.png")`,
+            }}
+            className={`${spritesStyle.sun} fixed pointer-events-none select-none -z-20 ${spritesStyle.innerSun4}`}
+          />
+          <div
+            style={{
+              backgroundImage: `url("./theme/animated-background/pixelland/sun/inner-5.png")`,
+            }}
+            className={`${spritesStyle.sun} fixed pointer-events-none select-none -z-20 ${spritesStyle.innerSun5}`}
+          />
+        </>
+      )}
       <div
         style={{
           backgroundImage: `url("./theme/animated-background/pixelland/pixel-clouds.png")`,
